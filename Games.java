7d6bfa5225475f0f85d433e0e32287c83773d41a
Sunilg66 <sunilkg251@gmail.com>
2021-05-07 12:49:38 +0530

    adding file

---
+++ Games.java
@@ -0,0 +1,13 @@
+class Games{
+strictfp final public static void main(String []n){
+System.out.println("started main method");
+short time=5;
+if (time>=5){
+System.out.println("playing game");
+}
+else {
+System.out.println("reading books");
+}
+System.out.println("end of main method");
+}
+}

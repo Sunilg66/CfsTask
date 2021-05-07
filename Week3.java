f254a64da5cde813bf16c8d4cc6596a6a86e3060
Sunilg66 <sunilkg251@gmail.com>
2021-05-07 12:46:53 +0530

    adding file

---
+++ Week3.java
@@ -0,0 +1,23 @@
+class Week3{
+  public static void main(String []n){
+    int day=7;
+    int a=15;
+    int b=(a % day);
+  switch(b){
+case 1:System.out.println("sunday");
+       break;
+case 2:System.out.println("monday");
+       break;
+case 3:System.out.println("tuesday");
+       break;
+case 4:System.out.println("wednesday");
+       break;
+case 5:System.out.println("thursday");
+       break;
+case 6:System.out.println("friday");
+       break;
+case 7:System.out.println("saturday");
+       break;
+}
+}
+}

03d5c22e318345e8ba10a65c5aebea7525fdabac
Sunilg66 <sunilkg251@gmail.com>
2021-05-07 12:50:33 +0530

    adding files

---
+++ Evennumbers.java
@@ -0,0 +1,17 @@
+class Evennumbers{
+
+public static void main(String []n){
+
+int number=100;
+
+System.out.println("even numbers are: ");
+
+for(int i=1; i<=number;i++){
+
+if(i%2==0){
+
+System.out.print(i+" " );
+}
+}
+}
+}
